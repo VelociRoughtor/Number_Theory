78984138bc04b7e2d9c0ba35be51e40262421aff
YourUsername <mdmehedihasanrafy67865@gmail.com>
2025-02-08 18:39:00 +0600

    Matrix Exponentiation is added

---
+++ Matrix_Exponentiation/Naive_Approach.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define v32 vector<int>
+#define vv32 vector<vector<int>>
+
+vv32 multi(vv32 A, vv32 B) {
+    int n = A.size();
+    vv32 C(n, v32(n, 0)); 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j]; 
+            }
+        }
+    }
+    return C;
+}
+
+vv32 result(vv32 v, int exp) {
+    int n = v.size();
+    vv32 res(n, v32(n, 0));
+
+    for (int i = 0; i < n; i++) {
+        res[i][i] = 1;
+    }
+
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            res = multi(res, v);
+        }
+        v = multi(v, v);
+        exp /= 2;
+    }
+
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vv32 mat(n, v32(n));
+
+    // Input matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    int exp;
+    cin >> exp;
+
+    vv32 res = result(mat, exp);
+
+    // Output result matrix
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << res[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
